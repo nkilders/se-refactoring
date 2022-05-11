7b3bf8209b2b67d3e03f012261b9647135563f71
nkilders <noah.kilders@gmail.com>
2022-05-11 15:17:12 +0200

    Move Method

--- src/main/java/Movie.java
+++ src/main/java/Movie.java
@@ -22,4 +22,33 @@ public class Movie {
     public String getTitle (){
         return title;
     }
+
+    public double getCharge(int daysRented) {
+        double charge = 0;
+
+        switch (this.priceCode) {
+            case Movie.REGULAR:
+                charge += 2;
+
+                if (daysRented > 2)
+                    charge += (daysRented - 2) * 1.5;
+
+                break;
+
+            case Movie.NEW_RELEASE:
+                charge += daysRented * 3;
+
+                break;
+
+            case Movie.CHILDREN:
+                charge += 1.5;
+
+                if (daysRented > 3)
+                    charge += (daysRented - 3) * 1.5;
+
+                break;
+        }
+
+        return charge;
+    }
 }
